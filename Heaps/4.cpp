fd941b1988ec794438f6e4d656df0ba0dcf095ed
Mohit Dharmik <dharmikmohit@gmail.com>
2022-04-06 02:33:33 +0530

    alternate solution of heap 4

--- Heaps/4.cpp
+++ Heaps/4.cpp
@@ -4,7 +4,32 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-class Solution{
+class Solution_HEAP{
+    
+public:	
+	vector<int> kLargest(int arr[], int n, int k) {
+	    
+	    vector<int> result(arr, arr+k);
+	    make_heap(result.begin(), result.end(), greater<int>());
+	    
+	    for(int i = k; i < n; i++) {
+	        
+	        if(result[0] < arr[i]) {
+	            
+	            pop_heap(result.begin(), result.end(), greater<int>());
+	            result[k - 1] = arr[i];
+	            push_heap(result.begin(), result.end(), greater<int>());
+	        }
+	    }
+	    
+	    sort_heap(result.begin(), result.end(), greater<int>());
+	    return result;
+	    
+	}
+
+};
+
+class Solution_PRIORITY_QUEUE{
     
 public:	
 	vector<int> kLargest(int arr[], int n, int k) {
