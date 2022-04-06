2bbd460c3179883878d33b4b161fb5295aadd39b
Mohit Dharmik <dharmikmohit@gmail.com>
2022-04-06 11:56:52 +0530

    optimised heap 10

--- Heaps/10.cpp
+++ Heaps/10.cpp
@@ -25,7 +25,7 @@ struct compare {
     }
 };
 
-class Solution {
+class Solution_unefficient_space {
   public:
     //Function to merge K sorted linked list.
     Node * mergeKLists(Node *arr[], int K) {
@@ -53,5 +53,59 @@ class Solution {
         curr->next = NULL;
         
         return head->next;
+    }
+};
+
+
+class Solution_constant_space {
+    
+    Node* merge(Node* a, Node* b) {
+        
+        Node* head = new Node(-1);
+        Node* curr = head;
+        
+        while(a && b) {
+            
+            if(a->data < b->data) {
+                
+                curr->next = a;
+                a = a->next;
+            }
+            else {
+                
+                curr->next = b;
+                b = b->next;
+            }
+            
+            curr = curr->next;
+        }
+        if(a) 
+            curr->next = a;
+        else 
+            curr->next = b;
+            
+        return head->next;
+    }
+  public:
+    //Function to merge K sorted linked list.
+    Node * mergeKLists(Node *arr[], int k) {
+        
+        while(k > 1) {
+            
+            int i = 0;
+            while((i << 1) < k) {
+                
+                if((i << 1) + 1 < k)
+                    arr[i] = merge(arr[(i << 1)], arr[(i << 1) + 1]);
+                else
+                    arr[i] = arr[(i << 1)];
+                
+                i++;
+            }   
+            
+            k = (k + 1)>>1;
+        }
+        
+        return arr[0];
     }
 };
