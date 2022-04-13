deaab444f42002cc6f21a9c586c071f06beb3be8
Mohit Dharmik <dharmikmohit@gmail.com>
2022-04-13 13:29:37 +0530

    updated bst 7

--- Binary Search Tree/7.cpp
+++ Binary Search Tree/7.cpp
@@ -1,5 +1,5 @@
 // LCA
-// https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
+// https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
 
 #include<bits/stdc++.h>
 using namespace std;
@@ -15,17 +15,18 @@ class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         
-        if(!root || root == p || root == q)
-            return root;
+        while(root) {
+            
+            if(root->val > p->val && root->val > q->val)
+                root = root->left;
+            
+            else if(root->val < p->val && root->val < q->val)
+                root = root->right;
+            
+            else 
+                return root;
+        }
         
-        TreeNode* l = lowestCommonAncestor(root->left, p, q);
-        TreeNode* r = lowestCommonAncestor(root->right, p, q);
-        
-        if(!l)
-            return r;
-        if(!r)
-            return l;
-       
         return root;
     }
 };
