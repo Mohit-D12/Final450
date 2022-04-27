e44eef9e56de556e3b652dff659dbdf58193ce1a
Mohit Dharmik <dharmikmohit@gmail.com>
2022-04-27 18:06:10 +0530

    backtrack 1 changes

--- Backtracking/1.cpp
+++ Backtracking/1.cpp
@@ -5,84 +5,52 @@
 using namespace std;
 
 class Solution{
+
+    vector<string> result;
+    string path;
     
-    bool isSafe(vector<vector<int>> &m, vector<string> &ans, vector<vector<bool>> &visited, int n, int i, int j)
-    {
-        return ((i >= 0 and i < n) && (j >= 0 && j < n) && !visited[i][j] && m[i][j]);
-    }
-    
-    void backtrack(vector<vector<int>> &m, vector<string> &ans, vector<vector<bool>> &visited, int n, int i, int j, string &path)
-    {
-        //base case
-        if(i == n-1 && j == n-1)
-        {
-            ans.push_back(path);
+    void findPathUtil(vector<vector<int>>& m, int n, int i, int j) {
+        
+        if(i == -1 || j == -1 || i == n || j == n || m[i][j] != 1)
+            return;
+            
+        if(i == n - 1 && i == j) {
+            
+            result.push_back(path);
             return;
         }
         
-        visited[i][j] = 1;
+        m[i][j] = 2;
         
-        int x, y;
+        int sz = path.size();
         
-        //down
-        x = i + 1;
-        y = j;
-        if(isSafe(m, ans, visited, n, x, y))
-        {
-            path.push_back('D');
-            backtrack(m, ans, visited, n, x, y, path);
-            path.pop_back();
-        }
+        path.push_back('U');
+        findPathUtil(m, n, i - 1, j);
         
-        //right
-        x = i;
-        y = j + 1;
-        if(isSafe(m, ans, visited, n, x, y))
-        {
-            path.push_back('R');
-            backtrack(m, ans, visited, n, x, y, path);
-            path.pop_back();
-        }
-        //up
-        x = i - 1;
-        y = j;
-        if(isSafe(m, ans, visited, n, x, y))
-        {
-            path.push_back('U');
-            backtrack(m, ans, visited, n, x, y, path);
-            path.pop_back();
-        }
-        //left
-        x = i;
-        y = j - 1;
-        if(isSafe(m, ans, visited, n, x, y))
-        {
-            path.push_back('L');
-            backtrack(m, ans, visited, n, x, y, path);
-            path.pop_back();
-        }
+        path[sz] = 'D';
+        findPathUtil(m, n, i + 1, j);
         
-        visited[i][j] = 0;
+        path[sz] = 'L';
+        findPathUtil(m, n, i, j - 1);
         
+        path[sz] = 'R';
+        findPathUtil(m, n, i, j + 1);
+        
+        path.pop_back();
+        m[i][j] = 1;
     }
     
     public:
     vector<string> findPath(vector<vector<int>> &m, int n) {
         // Your code goes here
-        vector<string> ans;
-        if(m[0][0] == 0)
-            return ans;
         
-        vector<vector<bool>> visited;
-        for(int i = 0; i < n; i++)
-        {
-            vector<bool> row(n, 0);
-            visited.push_back(row);
-        }
-            
-        string path = "";
-        backtrack(m, ans, visited, n, 0, 0, path);
-        return ans;
+        result.clear();
+        path = "";
+        
+        if(m[0][0] == 0 || m[n - 1][n - 1] == 0)
+            return result;
+        
+        findPathUtil(m, n, 0, 0);
+        return result;
     }
-    
 };
